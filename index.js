9c7e1907697d084ab3eeff569ac13cc83e2f3b68
elmin1122 <elmin.allahverdiyev.apple@gmail.com>
2023-10-07 14:56:03 -0700

    password validation added

--- index.js
+++ index.js
@@ -92,6 +92,21 @@ registerBtnElement.onclick = () => {
         }
     };
 
+    function validatePassword() {
+        var newPassword = document.getElementById('user-password').newPassword.value;
+        var minNumberofChars = 6;
+        var maxNumberofChars = 16;
+        var regularExpression  = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+        alert(newPassword); 
+        if(newPassword.length < minNumberofChars || newPassword.length > maxNumberofChars){
+            return false;
+        }
+        if(!regularExpression.test(newPassword)) {
+            alert("password should contain at least one number and one special character");
+            return false;
+        }
+    }
+
 };
 
 loginBtnElement.onclick = () =>{
